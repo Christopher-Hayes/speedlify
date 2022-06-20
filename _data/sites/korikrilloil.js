7bc873872a65c56cfc4c3cefeb005a24931635b4
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-06-20 13:42:36 -0400

    Update korikrilloil.js

--- _data/sites/korikrilloil.js
+++ _data/sites/korikrilloil.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'Kori Krill Oil',
 	description: 'Kori Krill Oil Craft+Shopify sites',
 	options: {
-		frequency: 60 * 23, // 23 hours
+		frequency: 60 * 24 * 4, // Every 4 days
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
@@ -10,15 +10,18 @@ module.exports = {
 		freshChrome: 'site'
 	},
 	urls: [
-		// Craft website
+		// * Craft website
 		// Homepage
 		'https://korikrilloil.com/',
 		// Why Kori page
 		'https://www.korikrilloil.com/why-kori-krill-oil/superior-omega-3',
-		// Shopify website
+		// Benefits
+		'https://www.korikrilloil.com/why-omega-3/health-benefits',
+
+		// * Shopify website
 		// "Homepage"
 		'https://shop.korikrilloil.com/',
 		// Product page
-		'https://shop.korikrilloil.com/collections/all-products/products/standard-1200-mg-softgels',
+		'https://shop.korikrilloil.com/products/standard-1200-mg-softgels',
 	]
 };
