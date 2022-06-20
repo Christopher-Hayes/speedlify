eb58f817b677cce75e39b57e93a93a684fb141c3
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-06-20 13:42:57 -0400

    Update ds.js

--- _data/sites/ds.js
+++ _data/sites/ds.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'Digital Surgeons',
 	description: 'Digital Surgeons\' main agency website',
 	options: {
-		frequency: 60 * 23, // 23 hours
+		frequency: 60 * 24 * 4, // Every 4 days
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
