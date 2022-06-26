f701922821dfaad43dbe8d375ca21d3515c89381
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-06-26 15:20:12 -0400

    Update korikrilloil.js

--- _data/sites/korikrilloil.js
+++ _data/sites/korikrilloil.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'Kori Krill Oil',
 	description: 'Kori Krill Oil Craft+Shopify sites',
 	options: {
-		frequency: 60 * 24 * 4, // Every 4 days
+		frequency: 60 * 24 * 1, // At most - every 1 day
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
